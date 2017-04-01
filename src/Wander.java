6b83647267a6f807db69b45390dc700b570651cb
hu-zhiling-540 <hu23z@mtholyoke.edu>
2017-04-01 13:12:04 -0400

    try out

---
+++ src/Wander.java
@@ -0,0 +1,25 @@
+import lejos.robotics.*;
+import lejos.robotics.subsumption.*; 
+import lejos.nxt.*;
+
+public class Wander implements Behavior{
+
+	@Override
+	public boolean takeControl() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void action() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void suppress() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
