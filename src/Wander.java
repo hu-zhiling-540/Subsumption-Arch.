75d354409e0983d3dfbce2a68e75573dd108caf0
hu-zhiling-540 <hu23z@mtholyoke.edu>
2017-04-03 17:36:30 -0400

    Implemented the Wander class

--- src/Wander.java
+++ src/Wander.java
@@ -1,9 +1,19 @@
 import lejos.robotics.*;
+import lejos.robotics.navigation.DifferentialPilot;
+//import lejos.robotics.navigation.ArcMoveController;
 import lejos.robotics.subsumption.*; 
 import lejos.nxt.*;
 
 public class Wander implements Behavior{
-
+	
+	public boolean isOn = true;
+	private DifferentialPilot robot;
+//	private ArcMoveController robot;
+	
+	public Wander(DifferentialPilot robot)	{
+		this.robot = robot;
+	}
+	
 	@Override
 	public boolean takeControl() {
 		return true;
@@ -11,14 +21,22 @@ public class Wander implements Behavior{
 
 	@Override
 	public void action() {
-		// TODO Auto-generated method stub
-		
+		int angle = 0; // random
+		Motor.A.rotate(angle);
+		Motor.C.rotate(angle);
+		while (isOn)	{
+			Thread.yield();
+			Motor.A.stop();
+			Motor.C.stop();
+		}
+//		robot.forward(); 
+//		double randomAngle;	// neg to the right, pos to the left; from -180 to 180
+//		robot.travelArc(randomAngle, distance, immediateReturn);
 	}
 
 	@Override
 	public void suppress() {
-		// TODO Auto-generated method stub
-		
+		isOn = false;
 	}
 
 }
