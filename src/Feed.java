cfe47dee76f13c7ce00556d4bbe3060c257a6367
hu-zhiling-540 <hu23z@mtholyoke.edu>
2017-04-05 20:58:17 -0400

    minor fixup

--- src/Feed.java
+++ src/Feed.java
@@ -1,18 +1,24 @@
+import java.util.Timer;
+
 import lejos.nxt.LightSensor;
+import lejos.nxt.SensorPort;
+import lejos.robotics.navigation.DifferentialPilot;
 import lejos.robotics.subsumption.Behavior;
 
 public class Feed implements Behavior{
 	
-	Time timer;
+	Timer timer;
 	Boolean flag;
 	LightSensor light; 
 	Boolean floodlight;
+	DifferentialPilot robot;
 	
 	/**
 	 * Constructor create a light sensor object attached to the specified port, 
-	 * and sets floodlighting on or off.
+	 * and sets flood lighting on or off.
 	 */
-	public Feed(LightSensor light, boolean floodlight)	{
+	public Feed(LightSensor light, boolean floodlight, DifferentialPilot robot)	{
+		this.robot = robot;
 		this.light = light;
 		light = new LightSensor(SensorPort.S1);
 		
@@ -26,14 +32,24 @@ public class Feed implements Behavior{
 	public void action() {
 		// TODO Auto-generated method stub
 		// If the robot is over a food source 
-		if(lightsensor.getVlaue != 0) { 
+		if(light.getLightValue()!= 0) { 
 			// The robot should stop and feed for 3 seconds
 			robot.stop(); 
-			timer.schedule( )
-			
-			
+			timer.schedule(null, 0);
 			
 		}
+		
+	}
+
+	@Override
+	public boolean takeControl() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void suppress() {
+		// TODO Auto-generated method stub
 		
 	}
 }
